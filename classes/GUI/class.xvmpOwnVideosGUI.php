80284ee0e2bce77fa9565af0bead1991edb4a674
Ilja Lukin <ilja.lukin@fh-dortmund.de>
2023-12-19 10:15:07 +0100

    Fixes

--- classes/GUI/class.xvmpOwnVideosGUI.php
+++ classes/GUI/class.xvmpOwnVideosGUI.php
@@ -11,66 +11,69 @@ declare(strict_types=1);
  *
  * @ilCtrl_isCalledBy xvmpOwnVideosGUI: ilObjViMPGUI
  */
-class xvmpOwnVideosGUI extends xvmpVideosGUI {
+class xvmpOwnVideosGUI extends xvmpVideosGUI
+{
 
-	const SUBTAB_ACTIVE = xvmpVideosGUI::SUBTAB_OWN;
+    const SUBTAB_ACTIVE = xvmpVideosGUI::SUBTAB_OWN;
 
-	const TABLE_CLASS = 'xvmpOwnVideosTableGUI';
+    const TABLE_CLASS = 'xvmpOwnVideosTableGUI';
 
-	const CMD_EDIT_VIDEO = 'editVideo';
-	const CMD_CHANGE_OWNER = 'changeOwner';
-	const CMD_CONFIRMED_CHANGE_OWNER = 'confirmedChangeOwner';
-	const CMD_UPDATE_VIDEO = 'updateVideo';
-	const CMD_DELETE_VIDEO = 'deleteVideo';
-	const CMD_UPLOAD_VIDEO_FORM = 'uploadVideoForm';
-	const CMD_CREATE = 'create';
-	const CMD_CONFIRMED_DELETE_VIDEO = 'confirmedDeleteVideo';
-	const CMD_UPLOAD_CHUNKS = 'uploadChunks';
+    const CMD_EDIT_VIDEO = 'editVideo';
+    const CMD_CHANGE_OWNER = 'changeOwner';
+    const CMD_CONFIRMED_CHANGE_OWNER = 'confirmedChangeOwner';
+    const CMD_UPDATE_VIDEO = 'updateVideo';
+    const CMD_DELETE_VIDEO = 'deleteVideo';
+    const CMD_UPLOAD_VIDEO_FORM = 'uploadVideoForm';
+    const CMD_CREATE = 'create';
+    const CMD_CONFIRMED_DELETE_VIDEO = 'confirmedDeleteVideo';
+    const CMD_UPLOAD_CHUNKS = 'uploadChunks';
 
 
-	protected function performCommand($cmd) {
-		switch ($cmd) {
-			case self::CMD_EDIT_VIDEO:
-			case self::CMD_CHANGE_OWNER:
-			case self::CMD_UPDATE_VIDEO:
-			case self::CMD_DELETE_VIDEO:
-			case self::CMD_CONFIRMED_CHANGE_OWNER:
-			case self::CMD_CONFIRMED_DELETE_VIDEO:
-				$mid = max($_GET['mid'], $_POST['mid']);
-				$medium = xvmpMedium::find($mid);
-				ilObjViMPAccess::checkAction(ilObjViMPAccess::ACTION_MANIPULATE_VIDEO, $this, $medium);
-				break;
-			case self::CMD_FILL_MODAL:
-				$mid = max($_GET['mid'], $_POST['mid']);
-				$medium = xvmpMedium::find($mid);
-				ilObjViMPAccess::checkAction(ilObjViMPAccess::ACTION_PLAY_VIDEO, $this, $medium);
-				break;
-			default:
-				if (!ilObjViMPAccess::hasWriteAccess() && !ilObjViMPAccess::hasUploadPermission()) {
-					$this->accessDenied();
-				}
-		}
-		if ($cmd != self::CMD_UPLOAD_CHUNKS) {
-			/**
-			 * this will find (and cache) or create a vimp user,
-			 * or throw an exception if no vimp user is found and no vimp user can be created.
-			 */
-			xvmpUser::getOrCreateVimpUser($this->dic->user());
-		}
-		parent::performCommand($cmd);
-	}
+    protected function performCommand($cmd)
+    {
+        switch ($cmd) {
+            case self::CMD_EDIT_VIDEO:
+            case self::CMD_CHANGE_OWNER:
+            case self::CMD_UPDATE_VIDEO:
+            case self::CMD_DELETE_VIDEO:
+            case self::CMD_CONFIRMED_CHANGE_OWNER:
+            case self::CMD_CONFIRMED_DELETE_VIDEO:
+                $mid = max($_GET['mid'], $_POST['mid']);
+                $medium = xvmpMedium::find($mid);
+                ilObjViMPAccess::checkAction(ilObjViMPAccess::ACTION_MANIPULATE_VIDEO, $this, $medium);
+                break;
+            case self::CMD_FILL_MODAL:
+                $mid = max($_GET['mid'], $_POST['mid']);
+                $medium = xvmpMedium::find($mid);
+                ilObjViMPAccess::checkAction(ilObjViMPAccess::ACTION_PLAY_VIDEO, $this, $medium);
+                break;
+            default:
+                if (!ilObjViMPAccess::hasWriteAccess() && !ilObjViMPAccess::hasUploadPermission()) {
+                    $this->accessDenied();
+                }
+        }
+        if ($cmd != self::CMD_UPLOAD_CHUNKS) {
+            /**
+             * this will find (and cache) or create a vimp user,
+             * or throw an exception if no vimp user is found and no vimp user can be created.
+             */
+            xvmpUser::getOrCreateVimpUser($this->dic->user());
+        }
+        parent::performCommand($cmd);
+    }
 
 
     /**
      *
      * @throws ilCtrlException
      */
-	public function editVideo() {
-		$mid = $_GET['mid'];
-		$xvmpEditVideoFormGUI = new xvmpEditVideoFormGUI($this, $mid);
-		$xvmpEditVideoFormGUI->fillForm();
-		$this->dic->ui()->mainTemplate()->setContent($xvmpEditVideoFormGUI->getHTML());
-	}
+    public function editVideo()
+    {
+        $mid = $_GET['mid'];
+        $xvmpEditVideoFormGUI = new xvmpEditVideoFormGUI($this, $mid);
+        $xvmpEditVideoFormGUI->fillForm();
+        $this->dic->ui()->mainTemplate()->setContent($xvmpEditVideoFormGUI->getHTML());
+    }
 
 
     /**
@@ -78,51 +81,67 @@ class xvmpOwnVideosGUI extends xvmpVideosGUI {
      * @throws ilCtrlException
      * @throws xvmpException
      */
-	public function changeOwner() {
-		$mid = filter_input(INPUT_GET, 'mid');
-		$login = filter_input(INPUT_POST, 'login');
-		$login_exists = ilObjUser::_loginExists((string) $login);
-		if ($login && $login_exists) {
-			$ilConfirmationGUI = new ilConfirmationGUI();
-			$ilConfirmationGUI->setFormAction($this->dic->ctrl()->getFormAction($this));
-			$ilConfirmationGUI->setHeaderText($this->pl->txt('msg_warning_change_owner'));
-			$ilConfirmationGUI->addItem('mid', $mid, sprintf(
-				$this->pl->txt('confirmation_new_owner'),
-				xvmpMedium::find($mid)->getTitle(),
-				$login
-			));
-			$ilConfirmationGUI->addHiddenItem('login', $login);
-			$ilConfirmationGUI->setConfirm($this->dic->language()->txt('confirm'), self::CMD_CONFIRMED_CHANGE_OWNER);
-			$ilConfirmationGUI->setCancel($this->dic->language()->txt('cancel'), self::CMD_STANDARD);
-			$this->dic->ui()->mainTemplate()->setContent($ilConfirmationGUI->getHTML());
-		} else {
-			if ($login && !$login_exists) {
+    public function changeOwner()
+    {
+        $mid = filter_input(INPUT_GET, 'mid');
+        $login = filter_input(INPUT_POST, 'login');
+        $login_exists = ilObjUser::_loginExists((string)$login);
+        if ($login && $login_exists) {
+            $ilConfirmationGUI = new ilConfirmationGUI();
+            $ilConfirmationGUI->setFormAction($this->dic->ctrl()->getFormAction($this));
+            $ilConfirmationGUI->setHeaderText($this->pl->txt('msg_warning_change_owner'));
+            $ilConfirmationGUI->addItem('mid', $mid, sprintf(
+                $this->pl->txt('confirmation_new_owner'),
+                xvmpMedium::find($mid)->getTitle(),
+                $login
+            ));
+            $ilConfirmationGUI->addHiddenItem('login', $login);
+            $ilConfirmationGUI->setConfirm($this->dic->language()->txt('confirm'), self::CMD_CONFIRMED_CHANGE_OWNER);
+            $ilConfirmationGUI->setCancel($this->dic->language()->txt('cancel'), self::CMD_STANDARD);
+            $this->dic->ui()->mainTemplate()->setContent($ilConfirmationGUI->getHTML());
+        } else {
+            if ($login && !$login_exists) {
                 $this->dic->ui()->mainTemplate()->setOnScreenMessage('failure', $this->pl->txt('msg_error_login_not_found'), true);
-			}
-			$xvmpChangeOwnerFormGUI = new xvmpChangeOwnerFormGUI($this, $mid);
-			$this->dic->ui()->mainTemplate()->setContent($xvmpChangeOwnerFormGUI->getHTML());
-		}
-	}
+            }
+            $xvmpChangeOwnerFormGUI = new xvmpChangeOwnerFormGUI($this, $mid);
+            $this->dic->ui()->mainTemplate()->setContent($xvmpChangeOwnerFormGUI->getHTML());
+        }
+    }
 
 
-	/**
-	 *
-	 */
-	public function confirmedChangeOwner() {
-		$mid = (int) filter_input(INPUT_POST, 'mid');
-		$login = filter_input(INPUT_POST, 'login');
+    /**
+     *
+     */
+    public function confirmedChangeOwner()
+    {
+        $mid = (int)filter_input(INPUT_POST, 'mid');
+        $login = filter_input(INPUT_POST, 'login');
 
-		$medium = xvmpMedium::getObjectAsArray($mid);
-		if ($medium['uid'] !== xvmpUser::getVimpUser($this->dic->user())->getUid()) {
+        $medium = xvmpMedium::getObjectAsArray($mid);
+        if ($medium['uid'] !== xvmpUser::getVimpUser($this->dic->user())->getUid()) {
             $this->dic->ui()->mainTemplate()->setOnScreenMessage('failure', $this->pl->txt('permission_denied'), true);
-			$this->dic->ctrl()->redirect($this, self::CMD_STANDARD);
-		}
+            $this->dic->ctrl()->redirect($this, self::CMD_STANDARD);
+        }
 
-		$xvmpUser = xvmpUser::getOrCreateVimpUser(new ilObjUser(ilObjUser::getUserIdByLogin($login)));
-		$medium['uid'] = $xvmpUser->getUid();
-		$edit_fields = ['uid' => $xvmpUser->getUid(), 'mediapermissions' => implode(',', $medium['mediapermissions']['rid'])];
-		foreach (xvmpConf::getConfig(xvmpConf::F_FORM_FIELDS) as $form_field) {
-			// workaround for vimp bug (see PLVIMP-53)
+        $xvmpUser = xvmpUser::getOrCreateVimpUser(new ilObjUser(ilObjUser::getUserIdByLogin($login)));
+        $medium['uid'] = $xvmpUser->getUid();
+        $mediapermissions = array();
+
+        if (isset($medium['mediapermissions']['rid'])) {
+            $mediapermissions[] = $medium['mediapermissions']['rid'];
+        } elseif (isset($medium['mediapermissions'])) {
+            foreach ($medium['mediapermissions'] as $rid) {
+                $mediapermissions[] = $rid;
+            }
+        }
+
+        $edit_fields = [
+            'uid' => $xvmpUser->getUid(),
+            'mediapermissions' => implode(',', $mediapermissions),
+        ];
+
+        foreach (xvmpConf::getConfig(xvmpConf::F_FORM_FIELDS) as $form_field) {
+            // workaround for vimp bug (see PLVIMP-53)
             if (
                 isset($form_field[xvmpConf::F_FORM_FIELD_REQUIRED]) &&
                 isset($form_field[xvmpConf::F_FORM_FIELD_TYPE]) &&
@@ -131,121 +150,127 @@ class xvmpOwnVideosGUI extends xvmpVideosGUI {
             ) {
                 $edit_fields[$form_field[xvmpConf::F_FORM_FIELD_ID]] = 1;
             }
+        }
 
-		}
-		$response = xvmpRequest::editMedium($mid, $edit_fields)->getResponseBody();
-		if ($response) {
+        $response = xvmpRequest::editMedium($mid, $edit_fields)->getResponseBody();
+        if ($response) {
             $this->dic->ui()->mainTemplate()->setOnScreenMessage('info', $this->pl->txt('form_saved'), true);
-			xvmpCacheFactory::getInstance()->delete(xvmpMedium::class . '-' . $mid);
-			xvmpMedium::cache(xvmpMedium::class . '-' . $mid, $medium);
-			xvmpEventLog::logEvent(xvmpEventLog::ACTION_CHANGE_OWNER, $this->getObjId(), array(
-				'owner' => $login,
-				'mid' => $mid,
-				'title' => $medium['title']
-			));
-			/** @var xvmpUploadedMedia $xvmpUploadedMedia */
-			foreach (xvmpUploadedMedia::where(['mid' => $mid, 'user_id' => $this->dic->user()->getId()])->get() as $xvmpUploadedMedia) {
-				$new_user_id = ilObjUser::_lookupId($login);
-				$xvmpUploadedMedia->setUserId($new_user_id);
-				$xvmpUploadedMedia->setEmail(ilObjUser::_lookupEmail($new_user_id));
-				$xvmpUploadedMedia->update();
-			}
-		} else {
+            xvmpCacheFactory::getInstance()->delete(xvmpMedium::class . '-' . $mid);
+            xvmpMedium::cache(xvmpMedium::class . '-' . $mid, $medium);
+            xvmpEventLog::logEvent(xvmpEventLog::ACTION_CHANGE_OWNER, $this->getObjId(), array(
+                'owner' => $login,
+                'mid' => $mid,
+                'title' => $medium['title']
+            ));
+            /** @var xvmpUploadedMedia $xvmpUploadedMedia */
+            foreach (xvmpUploadedMedia::where(['mid' => $mid, 'user_id' => $this->dic->user()->getId()])->get() as $xvmpUploadedMedia) {
+                $new_user_id = ilObjUser::_lookupId($login);
+                $xvmpUploadedMedia->setUserId($new_user_id);
+                $xvmpUploadedMedia->setEmail(ilObjUser::_lookupEmail($new_user_id));
+                $xvmpUploadedMedia->update();
+            }
+        } else {
             $this->dic->ui()->mainTemplate()->setOnScreenMessage('failure', $this->pl->txt('failure'));
-		}
+        }
 
-		$this->dic->ctrl()->redirect($this, self::CMD_STANDARD);
-	}
+        $this->dic->ctrl()->redirect($this, self::CMD_STANDARD);
+    }
 
-	/**
-	 *
-	 */
-	public function updateVideo() {
-		$xvmpEditVideoFormGUI = new xvmpEditVideoFormGUI($this, $_POST['mid']);
-		$xvmpEditVideoFormGUI->setValuesByPost();
-		if ($xvmpEditVideoFormGUI->saveForm()) {
+    /**
+     *
+     */
+    public function updateVideo()
+    {
+        $xvmpEditVideoFormGUI = new xvmpEditVideoFormGUI($this, $_POST['mid']);
+        $xvmpEditVideoFormGUI->setValuesByPost();
+        if ($xvmpEditVideoFormGUI->saveForm()) {
             $this->dic->ui()->mainTemplate()->setOnScreenMessage('info', $this->pl->txt('form_saved'), true);
-			$this->dic->ctrl()->redirect($this, self::CMD_STANDARD);
-		}
+            $this->dic->ctrl()->redirect($this, self::CMD_STANDARD);
+        }
         $this->dic->ui()->mainTemplate()->setOnScreenMessage('failure', $this->pl->txt('msg_incomplete'));
-		$this->dic->ui()->mainTemplate()->setContent($xvmpEditVideoFormGUI->getHTML());
-	}
+        $this->dic->ui()->mainTemplate()->setContent($xvmpEditVideoFormGUI->getHTML());
+    }
 
-	/**
-	 *
-	 */
-	public function uploadVideoForm() {
-		$xvmpUploadVideoFormGUI = new xvmpUploadVideoFormGUI($this);
-		$xvmpUploadVideoFormGUI->fillForm();
-		$this->dic->ui()->mainTemplate()->setContent($xvmpUploadVideoFormGUI->getHTML());
-	}
+    /**
+     *
+     */
+    public function uploadVideoForm()
+    {
+        $xvmpUploadVideoFormGUI = new xvmpUploadVideoFormGUI($this);
+        $xvmpUploadVideoFormGUI->fillForm();
+        $this->dic->ui()->mainTemplate()->setContent($xvmpUploadVideoFormGUI->getHTML());
+    }
 
 
-	/**
-	 *
-	 */
-	public function create() {
-		$xvmpEditVideoFormGUI = new xvmpUploadVideoFormGUI($this);
-		$xvmpEditVideoFormGUI->setValuesByPost();
-		if ($xvmpEditVideoFormGUI->saveForm()) {
+    /**
+     *
+     */
+    public function create()
+    {
+        $xvmpEditVideoFormGUI = new xvmpUploadVideoFormGUI($this);
+        $xvmpEditVideoFormGUI->setValuesByPost();
+        if ($xvmpEditVideoFormGUI->saveForm()) {
             $this->dic->ui()->mainTemplate()->setOnScreenMessage('success', $this->pl->txt('video_uploaded'), true);
-			$this->dic->ctrl()->redirect($this, self::CMD_STANDARD);
-		}
+            $this->dic->ctrl()->redirect($this, self::CMD_STANDARD);
+        }
 
         $this->dic->ui()->mainTemplate()->setOnScreenMessage('failure', $this->pl->txt('form_incomplete'));
-		$xvmpEditVideoFormGUI->setValuesByPost();
-		$this->dic->ui()->mainTemplate()->setContent($xvmpEditVideoFormGUI->getHTML());
-	}
+        $xvmpEditVideoFormGUI->setValuesByPost();
+        $this->dic->ui()->mainTemplate()->setContent($xvmpEditVideoFormGUI->getHTML());
+    }
 
 
-	/**
-	 *
-	 */
-	public function deleteVideo() {
-		$mid = $_GET['mid'];
-		$video = xvmpMedium::find($mid);
-		$confirmation_gui = new ilConfirmationGUI();
-		$confirmation_gui->setFormAction($this->dic->ctrl()->getFormAction($this));
-		$confirmation_gui->setHeaderText($this->pl->txt('confirm_delete_text'));
-		$confirmation_gui->addItem('mid', $mid, $video->getTitle());
-		$confirmation_gui->setConfirm($this->dic->language()->txt('delete'),self::CMD_CONFIRMED_DELETE_VIDEO);
-		$confirmation_gui->setCancel($this->dic->language()->txt('cancel'), self::CMD_STANDARD);
-		$this->dic->ui()->mainTemplate()->setContent($confirmation_gui->getHTML());
-	}
+    /**
+     *
+     */
+    public function deleteVideo()
+    {
+        $mid = $_GET['mid'];
+        $video = xvmpMedium::find($mid);
+        $confirmation_gui = new ilConfirmationGUI();
+        $confirmation_gui->setFormAction($this->dic->ctrl()->getFormAction($this));
+        $confirmation_gui->setHeaderText($this->pl->txt('confirm_delete_text'));
+        $confirmation_gui->addItem('mid', $mid, $video->getTitle());
+        $confirmation_gui->setConfirm($this->dic->language()->txt('delete'), self::CMD_CONFIRMED_DELETE_VIDEO);
+        $confirmation_gui->setCancel($this->dic->language()->txt('cancel'), self::CMD_STANDARD);
+        $this->dic->ui()->mainTemplate()->setContent($confirmation_gui->getHTML());
+    }
 
 
-	/**
-	 *
-	 */
-	public function confirmedDeleteVideo() {
-		$mid = (int) $_POST['mid'];
+    /**
+     *
+     */
+    public function confirmedDeleteVideo()
+    {
+        $mid = (int)$_POST['mid'];
 
-		// fetch the video for logging purposes
-		$video = xvmpMedium::getObjectAsArray($mid);
+        // fetch the video for logging purposes
+        $video = xvmpMedium::getObjectAsArray($mid);
 
-		xvmpMedium::deleteObject($mid);
+        xvmpMedium::deleteObject($mid);
 
-		xvmpEventLog::logEvent(xvmpEventLog::ACTION_DELETE, $this->getObjId(), $video);
+        xvmpEventLog::logEvent(xvmpEventLog::ACTION_DELETE, $this->getObjId(), $video);
 
         $this->dic->ui()->mainTemplate()->setOnScreenMessage('success', $this->pl->txt('video_deleted'), true);
-		$this->dic->ctrl()->redirect($this, self::CMD_STANDARD);
-	}
+        $this->dic->ctrl()->redirect($this, self::CMD_STANDARD);
+    }
 
 
-	/**
-	 *
-	 */
-	protected function uploadChunks() {
-		$xoctPlupload = new xoctPlupload();
-		$tmp_id = filter_input(INPUT_GET, 'tmp_id', FILTER_SANITIZE_STRING);
+    /**
+     *
+     */
+    protected function uploadChunks()
+    {
+        $xoctPlupload = new xoctPlupload();
+        $tmp_id = filter_input(INPUT_GET, 'tmp_id', FILTER_SANITIZE_STRING);
 
-		$dir = ILIAS_ABSOLUTE_PATH  . ltrim(ilFileUtils::getWebspaceDir(), '.') . '/vimp/' . $tmp_id;
-		if (!is_dir($dir)) {
+        $dir = ILIAS_ABSOLUTE_PATH . ltrim(ilFileUtils::getWebspaceDir(), '.') . '/vimp/' . $tmp_id;
+        if (!is_dir($dir)) {
             ilFileUtils::makeDir($dir);
-		}
+        }
 
-		$xoctPlupload->setTargetDir($dir);
-		$xoctPlupload->handleUpload();
-	}
+        $xoctPlupload->setTargetDir($dir);
+        $xoctPlupload->handleUpload();
+    }
 
 }
