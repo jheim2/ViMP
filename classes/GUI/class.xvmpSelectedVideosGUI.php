c30a190de9205ab5e6bb20d00c36ab0e91104260
Ilja Lukin <ilja.lukin@fh-dortmund.de>
2024-10-07 16:36:24 +0200

    Fix visibility setting

--- classes/GUI/class.xvmpSelectedVideosGUI.php
+++ classes/GUI/class.xvmpSelectedVideosGUI.php
@@ -7,80 +7,82 @@ declare(strict_types=1);
 /**
  * Class xvmpSelectedVideosGUI
  *
- * @author  Theodor Truffer <tt@studer-raimann.ch>
+ * @author            Theodor Truffer <tt@studer-raimann.ch>
  *
  * @ilCtrl_isCalledBy xvmpSelectedVideosGUI: ilObjViMPGUI
  */
-class xvmpSelectedVideosGUI extends xvmpVideosGUI {
+class xvmpSelectedVideosGUI extends xvmpVideosGUI
+{
 
-	const SUBTAB_ACTIVE = xvmpVideosGUI::SUBTAB_SELECTED;
+    const SUBTAB_ACTIVE = xvmpVideosGUI::SUBTAB_SELECTED;
 
-	const TABLE_CLASS = 'xvmpSelectedVideosTableGUI';
+    const TABLE_CLASS = 'xvmpSelectedVideosTableGUI';
 
-	const CMD_MOVE_UP = 'moveUp';
-	const CMD_MOVE_DOWN = 'moveDown';
-	const CMD_SET_VISIBILITY = 'setVisibility';
+    const CMD_MOVE_UP = 'moveUp';
+    const CMD_MOVE_DOWN = 'moveDown';
+    const CMD_SET_VISIBILITY = 'setVisibility';
 
+    public function executeCommand()
+    {
+        if (!ilObjViMPAccess::hasWriteAccess()) {
+            $this->accessDenied();
+        }
 
-	public function executeCommand() {
-		if (!ilObjViMPAccess::hasWriteAccess()) {
-			$this->accessDenied();
-		}
+        if (!$this->dic->ctrl()->isAsynch()) {
+            $this->addFlushCacheButton();
+        }
 
-		if (!$this->dic->ctrl()->isAsynch()) {
-			$this->addFlushCacheButton();
-		}
+        parent::executeCommand();
+    }
 
-		parent::executeCommand();
-	}
+    /**
+     * ajax
+     */
+    public function reorder()
+    {
+        $ids = $_POST['ids'];
+        $sort = 10;
+        foreach ($ids as $id) {
+            $xvmpSelectedMedia = xvmpSelectedMedia::where(array('mid' => $id, 'obj_id' => $this->getObjId()))->first();
+            $xvmpSelectedMedia->setSort($sort);
+            $xvmpSelectedMedia->update();
+            $sort += 10;
+        }
+        echo "{\"success\": true}";
+        exit;
+    }
 
+    /**
+     * ajax
+     */
+    public function moveUp()
+    {
+        $mid = $_GET['mid'];
+        xvmpSelectedMedia::moveUp($mid, $this->getObjId());
+        exit;
+    }
 
-	/**
-	 * ajax
-	 */
-	public function reorder() {
-		$ids = $_POST['ids'];
-		$sort = 10;
-		foreach ($ids as $id) {
-			$xvmpSelectedMedia = xvmpSelectedMedia::where(array('mid' => $id, 'obj_id' => $this->getObjId()))->first();
-			$xvmpSelectedMedia->setSort($sort);
-			$xvmpSelectedMedia->update();
-			$sort += 10;
-		}
-		echo "{\"success\": true}";
-		exit;
-	}
+    /**
+     * ajax
+     */
+    public function moveDown()
+    {
+        $mid = $_GET['mid'];
+        xvmpSelectedMedia::moveDown($mid, $this->getObjId());
+        exit;
+    }
 
-
-	/**
-	 * ajax
-	 */
-	public function moveUp() {
-		$mid = $_GET['mid'];
-		xvmpSelectedMedia::moveUp($mid, $this->getObjId());
-		exit;
-	}
-
-	/**
-	 * ajax
-	 */
-	public function moveDown() {
-		$mid = $_GET['mid'];
-		xvmpSelectedMedia::moveDown($mid, $this->getObjId());
-		exit;
-	}
-
-
-	/**
-	 * ajax
-	 */
-	public function setVisibility() {
-		$mid = $_GET['mid'];
-		$visible = $_GET['visible'];
-		/** @var xvmpSelectedMedia $video */
-		$video = xvmpSelectedMedia::where(array('mid' => $mid, 'obj_id' => $this->getObjId()))->first();
-		$video->setVisible($visible);
-		$video->update();
-		exit;
-	}
+    /**
+     * ajax
+     */
+    public function setVisibility()
+    {
+        $mid = $_GET['mid'];
+        $visible = $_GET['visible'];
+        /** @var xvmpSelectedMedia $video */
+        $video = xvmpSelectedMedia::where(array('mid' => $mid, 'obj_id' => $this->getObjId()))->first();
+        $video->setVisible((int) $visible);
+        $video->update();
+        exit;
+    }
 }
